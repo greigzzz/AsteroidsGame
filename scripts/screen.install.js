8fcb3535f21a20c7b97fdba1b0cae3edf98c3d2e
greigac <greigcolliar@gmail.com>
2018-02-22 22:55:39 +0000

    Initial commit

---
+++ scripts/screen.install.js
@@ -0,0 +1,6 @@
+//Similiar to slash screen setup. Clicking on install icon on a mobile safari for the game to be downloaded.
+titanic.screens["install-screen"] = (function() {
+    return {
+        run : function() {}
+    };
+})();
